a95c288464b1f4efdad45f62f4546b07e86ab7f1
Joshua <jpe7619@gmail.com>
2023-10-06 16:54:16 -0700

    install dependencies, establish three.js scene and render cube

---
+++ threejs-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ThreeScene from './ThreeScene';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React App test with Three.js render</h1>
+      </header>
+      <ThreeScene />
+    </div>
+  );
+}
+
+export default App;
---
+++ threejs-app/src/ThreeScene.js
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect } from 'react';
+import * as THREE from 'three';
+
+function ThreeScene() {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    // this creates the  scene, camera, and renderer
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+
+    // establish the renderer
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    canvasRef.current.appendChild(renderer.domElement);
+
+    // creates the test cube and add it to the scene
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    // settings for the initial camera position
+    camera.position.z = 5;
+
+    // actual animation settings
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    //clean and  unmount
+    return () => {
+      renderer.dispose();
+    };
+  }, []);
+
+  return <div ref={canvasRef} />;
+}
+
+export default ThreeScene;
